b2a37d6c9321d54e151abad5732a98f5f4bb9019
Sattar Hummatli <shummatli@Sattars-MacBook-Pro.local>
2020-02-21 17:04:51 +0400

    Add new card view

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+const bottomContainerHeight = 80.0;
+const color_card_view = Color(0xFF201F33);
+const color_bottom_container = Color(0xFFE22A60);
 
 class InputPage extends StatefulWidget {
   @override
@@ -13,34 +18,47 @@ class _InputPageState extends State<InputPage> {
         title: Text('BMI CALCULATOR'),
       ),
       body: SafeArea(
+        bottom: false,
         child: Column(
           children: <Widget>[
             Expanded(
               child: Row(
                 children: <Widget>[
                   MyCardView(
-                    color: Color(0xFF201F33),
+                    color: color_card_view,
+                    child: MyIconView(
+                      iconData: FontAwesomeIcons.mars,
+                      title: 'MALE',
+                    ),
                   ),
                   MyCardView(
-                    color: Color(0xFF201F33),
+                    color: color_card_view,
+                    child: MyIconView(
+                        title: 'FEMALE', iconData: FontAwesomeIcons.venus),
                   ),
                 ],
               ),
             ),
             MyCardView(
-              color: Color(0xFF201F33),
+              color: color_card_view,
             ),
             Expanded(
               child: Row(
                 children: <Widget>[
                   MyCardView(
-                    color: Color(0xFF201F33),
+                    color: color_card_view,
                   ),
                   MyCardView(
-                    color: Color(0xFF201F33),
+                    color: color_card_view,
                   ),
                 ],
               ),
+            ),
+            Container(
+              color: color_bottom_container,
+              margin: EdgeInsets.only(top: 10.0),
+              width: double.infinity,
+              height: bottomContainerHeight,
             ),
           ],
         ),
@@ -49,15 +67,45 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
+class MyIconView extends StatelessWidget {
+  final IconData iconData;
+  final String title;
+
+  MyIconView({@required this.title, @required this.iconData});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          iconData,
+          size: 80.0,
+        ),
+        SizedBox(height: 15.0),
+        Text(
+          title,
+          style: TextStyle(
+            color: Color(0xFF8D8E98),
+            fontSize: 18.0,
+          ),
+        )
+      ],
+    );
+  }
+}
+
 class MyCardView extends StatelessWidget {
   final Color color;
+  final Widget child;
 
-  MyCardView({@required this.color});
+  MyCardView({@required this.color, this.child});
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
       child: Container(
+        child: child,
         margin: EdgeInsets.all(15.0),
         decoration: BoxDecoration(
           color: color,
